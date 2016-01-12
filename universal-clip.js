c35752c31d83a7e88eb7479c6f914a96490cb7a1
Vijay Chandar <reachvijaychandar@gmail.com>
2016-01-12 22:33:24 +0530

    db insert form complete

--- universal-clip.js
+++ universal-clip.js
@@ -1,3 +1,5 @@
+Snippets = new Mongo.Collection("snippets");
+
 if (Meteor.isServer) {
 
 }
@@ -7,4 +9,23 @@ if (Meteor.isClient) {
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
   });
+  Template.afterLogIn.events({
+    "submit #new-snippet":function (event){
+      event.preventDefault();
+      var text = event.target.text.value;
+      Meteor.call("addSnip",text);
+      event.target.text.value = '';
+    }
+  });
 }
+
+Meteor.methods({
+  addSnip: function(text){
+      Snippets.insert({
+        text: text,
+        owner: Meteor.userId(),
+        createdAt: new Date()
+      });
+
+  }
+});
